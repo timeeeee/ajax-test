ec915e5f5c3a17a782599eacac024bcd438b2c1b
Tim Clark <timeeeee@gmail.com>
2016-11-24 16:38:26 -0500

    simpler ajax script, wait a couple seconds before loading

--- static/script.js
+++ static/script.js
@@ -1,13 +1,15 @@
-$(function ready() {
-  $.get({
-    url: "http://127.0.0.1:5000/api",
-    headers: {"Access-Control-Allow-Origin": "*"},
-    context: $("body"),
-    success: function ajax_success(data) {
-      $( this ).append($("<p>" + data + "</p"));
-    },
-    error: function ajax_error() {
-      $( this ).append($("<p>there was an error</p>"));
-    }
-  })
-});
+function ready() {
+  setTimeout(function load_ajax() {
+    $.get({
+      url: "/api",
+      success: function ajax_success(data) {
+        $("p#target").text(data);
+      },
+      error: function ajax_error() {
+        $("p#target").text("There was an error.");
+      }
+    });
+  }, 2000);
+}
+
+$(ready);
